063e24115f5968b76872c84b30f252b29526d30f
John O'connor <thien.hieu2299@gmail.com>
2023-06-12 23:03:46 +0700

    add player

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import UserProvider from '@/providers/UserProvider'
 import ModalProvider from '@/providers/ModalProvider'
 import ToasterProvider from '@/providers/ToasterProvider'
 import getSongsByUserId from '@/actions/getSongsByUserId'
+import Player from '@/components/Player'
 const font = Figtree({ subsets: ['latin'] })
 
 export const metadata = {
@@ -29,6 +30,7 @@ export default async function RootLayout({ children, }: { children: React.ReactN
             <Sidebar songs={userSongs}>
               {children}
             </Sidebar>
+            <Player />
           </UserProvider>
         </SupabaseProvider>
 
